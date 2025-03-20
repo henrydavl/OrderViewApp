2c907fd572d5d871124c5132b32143b45de3350e
Henry David Lie <henry.lie@ibm.com>
2025-03-20 10:31:14 +0700

    Initial Commit

---
+++ SatkomTest/SatkomTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatkomTestApp.swift
+//  SatkomTest
+//
+//  Created by Henry David Lie on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SatkomTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
